82d3a07f13d5d2e679fd083687a5cf44328e1be9
bedoshi <bedoshidiary.ester.freesia@gmial.com>
2021-09-15 17:07:00 +0900

    not working correct to show some DOMs.

---
+++ index2.js
@@ -0,0 +1,11 @@
+
+var app1000 = new Vue({
+    data: {
+        isActive: true,
+        hasError: false,
+        name: "ogino kotaro"
+    },
+    created: function () {
+        console.log('ogino kotaro')
+    }
+})
